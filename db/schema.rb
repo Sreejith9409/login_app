538b35b95d66dc6af1ee627acf2b8ef38c186a04
Sreejith <sreejith.s@bitlasoft.com>
2021-01-13 10:30:36 +0530

    Migration files pushed

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2021_01_12_132052) do
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "fname"
     t.string "lname"
     t.date "bdate"
